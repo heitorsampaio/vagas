f22b1d6ba37145acb415edd83e03e133d0295e60
Heitor Sampaio <horlando.heitor@gmail.com>
2020-10-19 13:07:18 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,17 +1,10 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-import uuid
+from django.shortcuts import render
+
+from .serializers import *
+from rest_framework.viewsets import ModelViewSet
+from .models import User
 
 
-class User(AbstractUser):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)
-    name = models.CharField("Name", max_length=255,
-                            help_text="Fullname of the Client",
-                            default=None)
-    cpf = models.CharField("CPF", unique=True, max_length=14,
-                           help_text="Valid CPF of the Client")
-    email = models.EmailField("Email", unique=True,
-                              help_text="Email of the Client")
-
-    class Meta:
-        ordering = ['id']
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
